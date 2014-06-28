f7dce2d191017812086e5c6b7ca1445b18d7937d
Deblyn Prado <deblynprado@gmail.com>
2014-06-28 20:25:44 -0300

    Adding comments

--- outdated-browser.php
+++ outdated-browser.php
@@ -15,11 +15,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function outdated_browser(){
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	* Define files path
+	*/
 	$outdated_browser_url =  plugin_dir_url( __FILE__ );
 	$outdated_css = $outdated_browser_url . 'css/outdatedBrowser.min.css';
 	$outdated_js = $outdated_browser_url . 'js/outdatedBrowser.min.js';
 	$outdated_main = $outdated_browser_url . 'js/main.js';
 	
+	
+	/**
+	* Setting the HTML content.
+	*/
 	echo ( '<div id="outdated"><h6>' );
 	echo __( 'Your browser is out-of-date!', 'outdated-browser' );
 	echo ('</h6><p>');
@@ -28,6 +42,9 @@ function outdated_browser(){
 	echo __( 'Update my browser now', 'outdated-browser');
 	echo ( '</a></p><p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p></div> ');
 
+	/**
+	* Calling styles and scripts with WP native functions
+	*/
 	wp_enqueue_style('outdated-browser-style', $outdated_css);
 	wp_enqueue_script( 'outdated-browser-js', $outdated_js);
 	wp_enqueue_script( 'outdated-browser-main', $outdated_main);	
