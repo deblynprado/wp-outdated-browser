da28354c1c39d666c8ea90e3aade7a6798868f7e
Deblyn Prado <deblynprado@gmail.com>
2014-06-28 22:09:27 -0300

    Using hook to add scripts and styles

--- outdated-browser.php
+++ outdated-browser.php
@@ -16,15 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function outdated_browser(){
 	/**
-	* Define files path
-	*/
-	$outdated_browser_url =  plugin_dir_url( __FILE__ );
-	$outdated_css = $outdated_browser_url . 'css/outdatedBrowser.min.css';
-	$outdated_js = $outdated_browser_url . 'js/outdatedBrowser.min.js';
-	$outdated_main = $outdated_browser_url . 'js/main.js';
-	
-	
-	/**
 	* Setting the HTML content.
 	*/
 	echo ( '<div id="outdated"><h6>' );
@@ -34,13 +25,24 @@ function outdated_browser(){
 	echo ( '<a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">' );
 	echo __( 'Update my browser now', 'outdated-browser');
 	echo ( '</a></p><p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p></div> ');
+}
 
+function outdated_scripts(){
+	/**
+	* Define files path
+	*/
+	$outdated_browser_url =  plugin_dir_url( __FILE__ );
+	$outdated_css = $outdated_browser_url . 'css/outdatedBrowser.min.css';
+	$outdated_js = $outdated_browser_url . 'js/outdatedBrowser.min.js';
+	$outdated_main = $outdated_browser_url . 'js/main.js';
+	
 	/**
 	* Calling styles and scripts with WP native functions
 	*/
 	wp_enqueue_style('outdated-browser-style', $outdated_css);
 	wp_enqueue_script( 'outdated-browser-js', $outdated_js);
-	wp_enqueue_script( 'outdated-browser-main', $outdated_main, array(), "1.0.0", true)
+	wp_enqueue_script( 'outdated-browser-main', $outdated_main, array(), "1.0.0", true);
 }
 
+add_action('wp_enqueue_scripts', 'outdated_scripts');
 add_action('wp_footer', 'outdated_browser');
