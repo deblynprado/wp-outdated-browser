c18ba2af1b7e03a761c5871c7b12a18eb1d94e88
Deblyn Prado <deblynprado@gmail.com>
2014-06-28 22:26:37 -0300

    Fixing WordPress Code Standards

--- outdated-browser.php
+++ outdated-browser.php
@@ -20,10 +20,10 @@ function outdated_browser(){
 	*/
 	echo ( '<div id="outdated"><h6>' );
 	echo __( 'Your browser is out-of-date!', 'outdated-browser' );
-	echo ('</h6><p>');
-	echo __( 'Update your browser to view this website correctly.', 'outdated-browser');
+	echo ( '</h6><p>' );
+	echo __( 'Update your browser to view this website correctly.', 'outdated-browser' );
 	echo ( '<a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">' );
-	echo __( 'Update my browser now', 'outdated-browser');
+	echo __( 'Update my browser now', 'outdated-browser' );
 	echo ( '</a></p><p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p></div> ');
 }
 
@@ -39,14 +39,14 @@ function outdated_scripts(){
 	/**
 	* Calling styles and scripts with WP native functions
 	*/
-	wp_enqueue_style('outdated-browser-style', $outdated_css);
+	wp_enqueue_style( 'outdated-browser-style', $outdated_css );
 	wp_enqueue_script( 'outdated-browser-js', $outdated_js);
-	wp_enqueue_script( 'outdated-browser-main', $outdated_main, array(), "1.0.0", true);
+	wp_enqueue_script( 'outdated-browser-main', $outdated_main, array(), "1.0.0", true );
 }
 
 function outdated_textdomain() {
 	load_plugin_textdomain( 'outdated-browser', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
 }
 
-add_action('wp_enqueue_scripts', 'outdated_scripts');
-add_action('wp_footer', 'outdated_browser');
+add_action( 'wp_enqueue_scripts', 'outdated_scripts' );
+add_action( 'wp_footer', 'outdated_browser' );
