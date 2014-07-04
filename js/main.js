9e18daf9e645850aa57d7b679c5ed54f0da3bdd3
Deblyn Prado <deblynprado@gmail.com>
2014-07-04 00:30:08 -0300

    Adding wpColorPicker config

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
+// Color Picker
+(function( $ ) {
+ 
+    // Add Color Picker to all inputs that have 'color-field' class
+    $(function() {
+        $('#font-color').wpColorPicker({
+            defaultColor: '#fff'
+        });
+
+        $('#bkg-color').wpColorPicker({
+            defaultColor: '#f25648'
+        });
+    });
+     
+})( jQuery );
+
 //PLAIN JAVASCRIPT
 //event listener form DOM ready
-function addLoadEvent(func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function() {
-            oldonload();
-            func();
-        }
-    }
-}
-//call plugin function after DOM ready
-addLoadEvent(
-    outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform'
-    })
-    );
-
-//USING jQuery
-$( document ).ready(function() {
-    outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform'
-    })
-})
+// function addLoadEvent(func) {
+//     var oldonload = window.onload;
+//     if (typeof window.onload != 'function') {
+//         window.onload = func;
+//     } else {
+//         window.onload = function() {
+//             oldonload();
+//             func();
+//         }
+//     }
+// }
+// //call plugin function after DOM ready
+// addLoadEvent(
+//     outdatedBrowser({
+//         bgColor: '#f25648',
+//         color: '#ffffff',
+//         lowerThan: 'transform'
+//     })
+// );
