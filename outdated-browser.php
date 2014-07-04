472a8c3773f6f9888d00bd676a045eb6e6cd6a74
Deblyn Prado <deblynprado@gmail.com>
2014-07-04 00:30:33 -0300

    Adding fields to Options Page

--- outdated-browser.php
+++ outdated-browser.php
@@ -14,6 +14,92 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+class AvalioParametros {
+ 
+    function __construct(){        
+        add_action('admin_menu', array( $this, 'avalio_parametros_page' ));
+    }
+ 
+    function avalio_parametros_page() {
+        global $avalio_parametros_admin;
+ 
+        $avalio_parametros_admin = add_submenu_page(
+            'options-general.php',               // The ID of the top-level menu page to which this submenu item belongs
+            __( 'Outdated Browser Options', 'outdated-browser' ),              // The value used to populate the browser's title bar when the menu page is active
+            'Outdated Browser',                              // The label of this submenu item displayed in the menu
+            'administrator',                        // What roles are able to access this submenu item
+            'outdated-browser-options',             // The ID used to represent this submenu item
+            array($this, 'avalio_parametros_admin')// The callback function used to render the options for this submenu item
+        );
+ 
+        /*
+        global $current_user;
+        $user_id = $current_user->ID;
+        $membership_levels = $current_user->membership_levels;
+ 
+        if($membership_levels[0]->name == 'Top'){
+            $avalio_parametros_top = add_submenu_page(
+                'avalio',               // The ID of the top-level menu page to which this submenu item belongs
+                'Plano Top',              // The value used to populate the browser's title bar when the menu page is active
+                'Plano Top',                              // The label of this submenu item displayed in the menu
+                'edit_posts',                        // What roles are able to access this submenu item
+                'avalio-parametros-top',             // The ID used to represent this submenu item
+                array($this, 'avalio_parametros_top')// The callback function used to render the options for this submenu item
+            );
+        }
+        */
+    }    
+
+    function avalio_parametros_admin() {
+        /*
+        if($_POST['opcoes']){
+            printr($_POST);
+        }
+        */
+        $opcoes = $_POST['opcoes'];
+        if( is_array($opcoes) ) {
+            update_option( 'outdated_background_color', $opcoes['background_color'] );
+            update_option( 'outdated_font_color', $opcoes['font_color'] );
+            update_option( 'outdated_language', $opcoes['lang'] );
+        }
+    ?>
+        <div class="wrap">
+            <div id="icon-tools" class="icon32"></div>
+            <h2><?php _e( 'Outdated Browser Settings','outdated-browser' )?></h2>
+            <br><br>
+            <form method="POST" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+ 
+                <table class="form-table">
+                    <tr valign="top">
+                        <td scope="row"><label for="background-color"><?php _e( 'Background Color', 'outdated-browser' ); ?></label></td>
+                        <td><input name="opcoes[bkg_color]" id="bkg-color" type="text" value="<?php echo get_option('outdated_background_color');?>" class="regular-text" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row"><label for="font-color"><?php _e( 'Font Color', 'outdated-browser' ); ?></label></td>
+                        <td><input name="opcoes[font_color]" id="font-color" type="text" value="<?php echo get_option('outdated_font_color');?>" class="regular-text" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row"><label for="select-language"><?php _e( 'Select Language', 'outdated-browser' ); ?></label></td>
+                        <td>
+                        	<select name="opcoes[lang]" id="select-language">                        		
+                        		<option value=""><?php _e( 'Select a language', 'outdated-browser' ); ?></option>
+                        		<option value="en" <?php selected( 'en' == get_option('lang') ); ?>><?php _e( 'English', 'outdated-browser' ); ?></option>
+                        		<option value="pt" <?php selected( 'pt' == get_option('lang') ); ?>><?php _e( 'Portuguese', 'outdated-browser' ); ?></option>
+                        	</select>
+                        </td>
+                    </tr>
+                </table>
+ 					<?php print_r($opcoes);?>
+                <br><br>
+                <input type="submit" class="button-primary" value="Salvar">
+            </form>
+        </div><!-- wrap -->
+    <?php
+		return array($opcoes);    
+    }
+    
+}
+
 function outdated_browser(){
 	/**
 	* Setting the HTML content.
@@ -50,3 +136,25 @@ function outdated_textdomain() {
 
 add_action( 'wp_enqueue_scripts', 'outdated_scripts' );
 add_action( 'wp_footer', 'outdated_browser' );
+
+///END of v1.0.0
+ 
+add_action( "init", "AvalioParametrosInit", 1 );
+ 
+function AvalioParametrosInit() {
+    global $AvalioParametros;
+    $AvalioParametros = new AvalioParametros();
+}
+// Color Picker
+add_action( 'admin_enqueue_scripts', 'wptuts_add_color_picker' );
+function wptuts_add_color_picker( $hook ) {
+ 
+    if( is_admin() ) {
+     
+        // Add the color picker css file      
+        wp_enqueue_style( 'wp-color-picker' );
+         
+        // Include our custom jQuery file with WordPress Color Picker dependency
+        wp_enqueue_script( 'custom-script-handle', plugins_url( 'js/main.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+    }
+}
