044595a4d113173a912df41ded053e83baa44af0
Deblyn Prado <deblynprado@gmail.com>
2014-07-05 21:49:36 -0300

    Calling colorPicker

---
+++ js/admin.js
@@ -0,0 +1,15 @@
+// Color Picker
+(function( $ ) {
+ 
+    // Add Color Picker to all inputs that have 'color-field' class
+    $(function() {
+        $('#font-color').wpColorPicker({
+            defaultColor: 'ffffff'
+        });
+
+        $('#bkg-color').wpColorPicker({
+            defaultColor: 'f25648'
+        });
+    });
+     
+})( jQuery );
