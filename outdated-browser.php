e359cfaa8eec2dda5f8975a19c605ad90d3a602e
Deblyn Prado <deblynprado@gmail.com>
2014-07-06 00:50:17 -0300

    Padronize comments

--- outdated-browser.php
+++ outdated-browser.php
@@ -15,17 +15,11 @@
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+	exit;
 }
 
 class OutdatedBrowser {
  
-	/**
-	* Constructor function
-	* @return: null
-	* @param: null
-	*/
-
 	function __construct(){
 		add_action( 'init', array( $this, 'outdated_textdomain' ) );
     add_action( 'admin_menu', array( $this, 'avalio_parametros_page' ) );
@@ -39,7 +33,7 @@ class OutdatedBrowser {
   function avalio_parametros_page() {
 	  global $outdated_param;
 	  $outdated_param = add_menu_page(
-	  	__( 'Outdated Options', 'outbws' ),	// Page Title
+	  	__( 'Outdated Options', 'outbws' ),						// Page Title
 	  	'Outdated Browser',														// Menu Name
 	  	'activate_plugins',														// Capabilities
 	  	'outdated-browser-options',										// Page Slug
@@ -48,16 +42,16 @@ class OutdatedBrowser {
 	  );	  
   }
 
-  // ------------------------------------------------------------------
-	 // Add all your sections, fields and settings during admin_init
-	 // ------------------------------------------------------------------
-	 //
+  /**
+	** Adding sections, fields and settings during admin_init
+	*/
+	 
 	 
 	 function outdated_settings_init() {
 	 	// Creating Sections
 	 	add_settings_section(
 			'outdated_general_section',										// Section id
-			__( 'General Options', 'outbws' ),	// Section title
+			__( 'General Options', 'outbws' ),						// Section title
 			array( $this, 'general_section_callback' ),		// Callback with Section HTML
 			'outdated-browser-options'										// Page slug where is section will apear
 		);
@@ -65,24 +59,24 @@ class OutdatedBrowser {
 	 	// Creating Settings
 	 	add_settings_field(
 			'background-color',														// Setting name
-			__( 'Background color', 'outbws' ),	// Setting Label
-			array( $this, 'color_setting_callback' ),						// Callback with the HTML of setting (any HTML element)
+			__( 'Background color', 'outbws' ),						// Setting Label
+			array( $this, 'color_setting_callback' ),			// Callback with the HTML of setting (any HTML element)
 			'outdated-browser-options',										// Slug the page where he will apear
 			'outdated_general_section'										// Section where he will apear
 		);
 
 		add_settings_field(
-			'font-color',														// Setting name
-			__( 'Font color', 'outbws' ),	// Setting Label
-			array( $this, 'font_setting_callback' ),						// Callback with the HTML of setting (any HTML element)
+			'font-color',																	// Setting name
+			__( 'Font color', 'outbws' ),									// Setting Label
+			array( $this, 'font_setting_callback' ),			// Callback with the HTML of setting (any HTML element)
 			'outdated-browser-options',										// Slug the page where he will apear
 			'outdated_general_section'										// Section where he will apear
 		);
 
 		add_settings_field(
 			'browser',																		// Setting name
-			__( 'More than', 'outbws' ),	// Setting Label
-			array( $this, 'browser_setting_callback' ),			// Callback with the HTML of setting (any HTML element)
+			__( 'More than', 'outbws' ),									// Setting Label
+			array( $this, 'browser_setting_callback' ),		// Callback with the HTML of setting (any HTML element)
 			'outdated-browser-options',										// Slug the page where he will apear
 			'outdated_general_section',										// Section where he will apear
 			$browsers = array(
@@ -100,24 +94,16 @@ class OutdatedBrowser {
 	 }
 	 
 	  
-	 // ------------------------------------------------------------------
-	 // Settings section callback function
-	 // ------------------------------------------------------------------
-	 //
-	 // This function is needed if we added a new section. This function 
-	 // will be run at the start of our section
-	 //
-	 
+	 /**
+	 ** Settings section callback function
+	 */	 
 	 function general_section_callback() {
 	 	echo __( 'Customize the message apearance', 'outbws' );
 	 }
 	 
-	 // ------------------------------------------------------------------
-	 // Callback function for our example setting
-	 // ------------------------------------------------------------------
-	 //
-	 // creates a checkbox true/false option. Other types are surely possible
-	 //
+	 /**
+	 ** Callback function for our example setting
+	 */
 	 
 	 function color_setting_callback() {
 	 	echo '<input name="background-color" id="bkg-color" type="text" value="' . get_option( 'background-color' ) . '" class="code"/>';	 	
@@ -133,8 +119,11 @@ class OutdatedBrowser {
 	 		echo '<option id="language" value="' . $key . '" ' . selected( get_option( 'browser' ), $key, true ) . '>' . $browser . '</option>';
 	 	endforeach;
 	 	echo '</select>';
-	 }	 
+	 }
 
+  /*
+  ** Function to print a form with all the options in admin page
+  */
   function print_html_options() { ?>
 		<form method="POST" action="options.php">
 			<?php settings_fields( 'outdated-browser-options' );
@@ -159,6 +148,9 @@ class OutdatedBrowser {
   	return($settings);
   }
 
+  /*
+  ** Using default WordPress erros messages
+  */
   function outdated_messages() {
   	settings_errors();
   }  
@@ -172,14 +164,17 @@ class OutdatedBrowser {
   	}
 	}
 
+	/*
+	** Default WordPress method to internationalize plugin
+	*/
 	function outdated_textdomain() {
 		load_plugin_textdomain( 'outbws', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
 	}	
 
-	function outdated_browser(){
-		/**
-		* Setting the HTML content.
-		*/
+	/**
+	* Put the Outdated Browser HTML in theme.
+	*/
+	function outdated_browser() {
 		echo ( '<div id="outdated"><h6>' );
 		echo __( 'Your browser is out-of-date!', 'outbws' );
 		echo ( '</h6><p>' );
@@ -190,6 +185,9 @@ class OutdatedBrowser {
 		$this->outdated_scripts($this->get_setting_saved());
 	}
 
+	/*
+	** Getting assets path and calling native functions of script and style.
+	*/
 	function outdated_scripts( $opt ){
 		/**
 		* Define files path
@@ -208,9 +206,12 @@ class OutdatedBrowser {
 		wp_localize_script( 'outdated-browser-main', 'outOptions', $opt);
 	}	
 }
-	
+
+/*
+** Init the __construct
+*/	
 add_action( "init", "OutdatedInit", 1 );
- 
+
 function OutdatedInit() {
   global $OutdatedBrowser;
   $OutdatedBrowser = new OutdatedBrowser();
