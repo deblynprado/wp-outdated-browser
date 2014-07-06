999ec5b747a68d92a350c12c2c4e1bc233e0a2d7
Deblyn Prado <deblynprado@gmail.com>
2014-07-06 02:46:54 -0300

    Updating Gruntfile with files that note used in WP

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     zip: {
-    	'<%= pkg.name %>.zip': ['**', '!*.zip', '!*.git', '!*.gitignore', '!*.DS_Store', '!*.md', '!*.json', '!Gruntfile.js', '!node_modules/**'],
+    	'<%= pkg.name %>.zip': ['**', '!*.zip', '!*.git', '!*.gitignore', '!*.DS_Store', '!*.md', '!*.json', '!Gruntfile.js', '!node_modules/**', '!assets/demo/**', '!assets/LICENSE', '!assets/README.md', '!assets/outdatedbrowser/outdatedBrowser.css', '!assets/outdatedbrowser/outdatedBrowser.js'],
     }    
   });
 
