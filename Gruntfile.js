6c90038dc4e494d911d0ab271048cd2510af1ff9
Deblyn Prado <deblynprado@gmail.com>
2015-08-24 16:59:23 -0300

    New plugin to compress source

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,29 @@
 module.exports = function(grunt) {
+	'use strict';
+	var gruntConfig = {
+		pkg: grunt.file.readJSON('package.json'),      
+    compress: {
+      main: {
+        options: {
+          archive: '<%= pkg.name %>-<%= pkg.version %>.zip'
+        },
+        files: [
+        {src:['**', '!*.zip', '!*.git', '!*.gitignore', '!*.DS_Store', '!*.md', '!*.json', '!Gruntfile.js', '!node_modules/**', '!original/**', '!PSDs/**']}
+      ]
+    }
+  }
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    zip: {
-    	'<%= pkg.name %>.zip': ['**', '!*.zip', '!*.git', '!*.gitignore', '!*.DS_Store', '!*.md', '!*.json', '!Gruntfile.js', '!node_modules/**', '!assets/demo/**', '!assets/LICENSE', '!assets/README.md', '!original/', '!*.psd'],
-    }    
-  });
+};
 
-  grunt.loadNpmTasks('grunt-zip');
+grunt.initConfig(gruntConfig);
 
-  grunt.registerTask('default', ['zip']);
+var keys = Object.keys(gruntConfig);
+var tasks = [];
+
+for(var i = 1, l = keys.length; i < l; i++) {
+  tasks.push(keys[i]);
+}
+grunt.loadNpmTasks('grunt-contrib-compress');
+grunt.registerTask('default', tasks);
 
 };
